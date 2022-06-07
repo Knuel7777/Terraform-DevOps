09366b569a62658831df04a4954a8453737306c3
cobregon <cobregon@imail.ag>
2022-06-06 20:48:04 -0600

    AWS Terraform

--- main.tf
+++ main.tf
@@ -1,34 +1,73 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
-}
-
-# Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
 }
 
-# Create a VPC
-resource "aws_vpc" "example" {
-  cidr_block = "10.0.0.0/16"
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"]
 }
 
-# Bucket S3
-resource "aws_s3_bucket" "b" {
-  bucket = "my-tf-test-bucket"
+variable "ssh_key_path" {}
+variable "vpc_id" {}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = file(var.ssh_key_path)
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description = "SSH from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
+    Name = "allow_ssh"
   }
 }
 
-resource "aws_s3_bucket_acl" "example" {
-  bucket = aws_s3_bucket.b.id
-  acl    = "private"
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+  key_name = aws_key_pair.deployer.key_name
+  vpc_security_group_ids = [
+    aws_security_group.allow_ssh.id
+  ]
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+output "ip_instance" {
+  value = aws_instance.web.public_ip
+}
+
+output "ssh" {
+  value = "ssh -l ubuntu ${aws_instance.web.public_ip}"
 }
 
